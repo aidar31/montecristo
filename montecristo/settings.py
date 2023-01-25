3112d80515fffc40e0a16b3d9f7d4fa60c6895b3
Aidar Isenov <120123042+aidar31@users.noreply.github.com>
2023-01-26 00:46:18 +0600

    Update settings.py

--- montecristo/settings.py
+++ montecristo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-58)jy7w5f2+)4&ggkxyih%&vd_!7zdmuh_hh7lmyen_=9y%x*#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['aidarisenov.live', 'montecristo.herokuapp.com'','127.0.0.1']
 
 
 # Application definition
